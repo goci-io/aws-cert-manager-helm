5ae5c42e33fd106a493cb7342954bbd663a3395b
etwillbefine <tim.gatzemeier@gmx.de>
2020-08-29 12:56:03 +0200

    use iam role module to simplify cert-manager module

--- main.tf
+++ main.tf
@@ -19,6 +19,18 @@ module "label" {
   name        = var.name
 }
 
+module "iam_role" {
+  source             = "git::https://github.com/goci-io/aws-iam-assumable-role.git?ref=master"
+  namespace          = var.namespace
+  stage              = var.stage
+  attributes         = var.attributes
+  name               = var.name
+  role_name_override = var.iam_role_name_override
+  trusted_iam_arns   = [var.cert_manager_trust_role]
+  trusted_services   = var.iam_role_trusted_services
+  policy_json        = data.aws_iam_policy_document.cert_manager.json
+}
+
 data "aws_route53_zone" "zone" {
   name         = format("%s.", var.dns_zone)
   private_zone = var.private_zone
